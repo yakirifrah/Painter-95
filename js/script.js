3ac94a989b2f5d51caeb4851a4eb906a88bd7d14
yakir <yakirifrah19@gmail.com>
2019-06-07 14:45:21 +0300

    adding eraser function

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@ var sideBarMenu = `<ul class="navbar-nav">
 //skeleton 
 var innerBody = `<div class="container">
                     <div class="row">
-                        <div id='canvas' class = 'col-md-11 canvas-style'></div>
+                        <div id='canvas' class = 'col-md-11 canvas-style' oncontextmenu="return false;"></div>
                     </div>
                         <div class = "row">
                         <div class = 'col-md-1'></div>
@@ -53,88 +53,79 @@ for (var i = lenPlateColorFirstRow; i < lenPlateColor; i++) {
     $('#second-row-plate').append(colButton);
 }
 
-{/* <i class="fas fa-pencil-altfa-pencil-alt"></i> */}
-// span id="id_' + i + '"
-{/* <i class="fas  "></i> */}
-for (var i = 0; i < lenFeatures/2; i++) {
-    if (i === 0) 
-    {
+for (var i = 0; i < lenFeatures / 2; i++) {
+    if (i === 0) {
         // console.log(typeof paint.features[i]);
-        var row = $('<div class="row"><button id = "' + paint.features[i]+ '" class="col-md-1 fas fa-eraser"></button><button id = "' + paint.features[i+1]+ '" class="col-md-1 fas fa-pencil-alt"></button></div>');
-    } 
+        var row = $('<div class="row"><button id = "' + paint.features[i] + '" class="col-md-1 fas fa-eraser"></button><button id = "' + paint.features[i + 1] + '" class="col-md-1 fas fa-pencil-alt"></button></div>');
+    }
 
-    if (i === 1)
-    {
-        var row = $('<div class="row"><button id = "' + paint.features[i+1]+ '" class="col-md-1 fas fa-brush"></button><button id = "' + paint.features[i+2]+ '" class="col-md-1 fas '+paint.features[i+2]+'"></button></div>');
+    if (i === 1) {
+        var row = $('<div class="row"><button id = "' + paint.features[i + 1] + '" class="col-md-1 fas fa-brush"></button><button id = "' + paint.features[i + 2] + '" class="col-md-1 fas ' + paint.features[i + 2] + '"></button></div>');
 
     }
     $("#container-nav-bar").append(row);
-    
+
 }
 
 
 
 $("button").click(function (e) {
-    console.log(e.button);
     if (e.button === 0) {
-   
+
         currentColor = e.target.id;
         $('#canvas').css('cursor', 'crosshair');
-        
+
     }
-   
 })
 
-
-// $("#eraser").$
-
-
-
-
-
-// $("#canvas").on('mousedown' , function(e)  {
-//     if (currentColor !== undefined) {
-//         // console.log("hight: "+ e.pageY);
-//         // console.log("width: "+e.pageX);
-//         var left = e.pageX - $('#canvas').offset().left +'px';
-//         var top = e.pageY - $('#canvas').offset().top +'px';
-//         console.log(left);
-//         console.log(top);
-//         var spanDraw = $('<span class="draw"></span>');
-//         // spanDraw.attr('class',`${currentColor}`);
-//         $(spanDraw).addClass(`${currentColor}`);
-//         $(spanDraw).css("left", `${left}`);
-//         $(spanDraw).css("top" ,`${top}`);
-
-//         $('#canvas').append(spanDraw);
-//     }
-// });
+var eraser = (eventObjectCanvas) => {
+    $(".draw").mousemove(function (e) {
+        e.target.remove();
+    })
+}
 
 
-$("#canvas").mousedown(function (e) {
-    if (e.button === 0) {
-        $(this).mousemove(function (e) {
+
+
+$("#canvas").mousedown(function (eventObjectCanvas) {
+    // console.log(e.button);
+
+    if (eventObjectCanvas.button === 0) {
+        $('#canvas').css('cursor', 'crosshair');
+        $(this).mousemove(function (eventObjectCanvas) {
             if (currentColor !== undefined) {
-                // console.log("hight: "+ e.pageY);
-                // console.log("width: "+e.pageX);
-                var left = e.pageX - $('#canvas').offset().left + 'px';
-                var top = e.pageY - $('#canvas').offset().top + 'px';
-                // console.log(left);
-                // console.log(top);
-                var spanDraw = $('<span class="draw"></span>');
-                // spanDraw.attr('class',`${currentColor}`);
+                var left = eventObjectCanvas.pageX - $('#canvas').offset().left + 'px';
+                var top = eventObjectCanvas.pageY - $('#canvas').offset().top + 'px';
+                var spanDraw = $('<span id="draw-span" class="draw"></span>');
                 $(spanDraw).addClass(`${currentColor}`);
                 $(spanDraw).css("left", `${left}`);
                 $(spanDraw).css("top", `${top}`);
-    
                 $('#canvas').append(spanDraw);
             }
         });
-        
-        
-    }
 
-   
+
+    }
 }).mouseup(function () {
     $(this).unbind('mousemove');
 });
+
+
+
+
+$("#canvas").mousedown(function (eventObjectCanvas) {
+    // console.log(e.button);
+    if (eventObjectCanvas.button === 2) {
+        var urlCursor = '../src/cursor/eraser.cur';
+        $(this).mousemove(function (eventObjectCanvas) {
+            $("#canvas").css('cursor', `url(${urlCursor}),auto`);
+
+            eraser(eventObjectCanvas);
+        });
+    }
+}).mouseup(function () {
+    $(this).unbind('mousemove');
+});
+
+
+
